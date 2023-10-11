ce974b6f0fd0cb7e22a6dd70c14b1de172c93bd0
smokiit <smo.nicek08@yahoo.com>
2023-10-11 15:35:08 +0900

    add 2,3,4 files

--- app/src/main/java/com/android/chap5/MainActivity.java
+++ app/src/main/java/com/android/chap5/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ex5_7);
+        setContentView(R.layout.ex5_3);
     }
 }
