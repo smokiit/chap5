d30f7d7fec82b367ae81e9f1e1ab85c4ee0e2838
smokiit <smo.nicek08@yahoo.com>
2023-10-11 14:50:54 +0900

    add ex5_1.xml rename all file to ex

--- app/src/main/java/com/android/chap5/MainActivity.java
+++ app/src/main/java/com/android/chap5/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.p5_6);
+        setContentView(R.layout.ex5_7);
     }
 }
