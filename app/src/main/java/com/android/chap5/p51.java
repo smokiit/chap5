2ec279733405d00821dfe863acd97b323a388798
smokiit <smo.nicek08@yahoo.com>
2023-10-17 15:52:07 +0900

    add p51

---
+++ app/src/main/java/com/android/chap5/p51.java
@@ -0,0 +1,14 @@
+package com.android.chap5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class p51 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p51);
+    }
+}
